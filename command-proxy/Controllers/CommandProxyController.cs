ccbb0576fae4827f1b6b209731ece3265164a4e7
Paul S <contact+github@winterei.se>
2018-04-20 16:15:15 +0900

    Add support for handling raw input (without caring about content type)

--- command-proxy/Controllers/CommandProxyController.cs
+++ command-proxy/Controllers/CommandProxyController.cs
@@ -1,24 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Spectero.Cproxy.Libraries.Utils;
 
 namespace Spectero.Cproxy.Controllers
 {
     //[Authorize(AuthenticationSchemes = "Bearer")]
     public class CommandProxyController : BaseController
     {
+        private readonly HttpRequest _request;
+
         public CommandProxyController(IOptionsMonitor<AppConfig> appConfig, ILogger<BaseController> logger,
             IHttpContextAccessor httpContext) : base(
             appConfig, logger, httpContext)
         {
+            _request = Context.Request;
         }
 
         public async Task<IActionResult> Handle()
         {
-            return Ok(Context.Request.Path);
+            var fullRequestedPath = _request.Path + _request.QueryString.Value;
+            var body = await StreamUtils.ReadStream(_request.Body);
+           
+
+            return Ok($"path: {fullRequestedPath}, body: {body}");
         }
     }
 }
---
+++ command-proxy/Libraries/Utils/StreamUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectero.Cproxy.Libraries.Utils
+{
+    public static class StreamUtils
+    {
+        public static async Task<string> ReadStream(Stream stream)
+        {
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
